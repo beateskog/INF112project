cd36fbc486a35cd8f0bafabc13d913414b1a5d93
krirogn <kf@krirogn.dev>
2023-02-16 12:45:34 +0100

    libGDX test

--- src/main/java/mavenless/skeleton/app/HelloWorld.java
+++ src/main/java/mavenless/skeleton/app/HelloWorld.java
@@ -8,8 +8,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 public class HelloWorld implements ApplicationListener {
+    public static final String FONT_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789][_!$%#@|\\/?-+=()*&.;,{}\"´`'<>";
+
     private SpriteBatch batch;
     private BitmapFont font;
     private Sprite sprite;
@@ -17,14 +21,21 @@ public class HelloWorld implements ApplicationListener {
     @Override
     public void create() {
         batch = new SpriteBatch();
+
+        // FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("mavenless/skeleton/assets/fonts/OpenSans/OpenSansRegular.ttf"));
+        // FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+        // parameter.size = 30;
+        // font = generator.generateFont(parameter);
+        // font.setColor(Color.BLACK);
+
         font = new BitmapFont();
         font.setColor(Color.RED);
 
         Texture texture = new Texture(Gdx.files.internal("mavenless/skeleton/assets/sprites/enemies/corona.png"));
-        sprite = new Sprite(texture, 0, 0, 16, 16);
-        sprite.setPosition(100, 100);
+        sprite = new Sprite(texture, 0, 0, 32, 32);
+        sprite.setPosition(400, 100);
         sprite.setRotation(0);
-        sprite.setSize(100, 100);
+        sprite.setSize(400, 400);
     }
 
     @Override
@@ -35,12 +46,12 @@ public class HelloWorld implements ApplicationListener {
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
         font.draw(batch, "Hello World", 200, 200);
-        
+
         sprite.draw(batch);
 
         batch.end();
@@ -48,13 +59,16 @@ public class HelloWorld implements ApplicationListener {
 
     @Override
     public void resize(int width, int height) {
+        System.out.println("The program is resized: " + width + " | " + height);
     }
 
     @Override
     public void pause() {
+        System.out.println("The program is paused");
     }
 
     @Override
     public void resume() {
+        System.out.println("The program is resumed");
     }
 }
--- src/main/java/mavenless/skeleton/app/Main.java
+++ src/main/java/mavenless/skeleton/app/Main.java
@@ -6,8 +6,8 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class Main {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
-        cfg.setTitle("hello-world");
-        cfg.setWindowedMode(480, 320);
+        cfg.setTitle("Rona Survivors");
+        cfg.setWindowedMode(1280, 720);
 
         new Lwjgl3Application(new HelloWorld(), cfg);
     }
