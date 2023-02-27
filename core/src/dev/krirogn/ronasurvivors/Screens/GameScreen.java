ecffcdede3c915842a507929dc80a84c2e6cc6f0
krirogn <kf@krirogn.dev>
2023-02-27 12:39:42 +0100

    Game tiles test

--- core/src/dev/krirogn/ronasurvivors/Screens/GameScreen.java
+++ core/src/dev/krirogn/ronasurvivors/Screens/GameScreen.java
@@ -1,12 +1,12 @@
 package dev.krirogn.ronasurvivors.Screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -24,10 +24,12 @@ public class GameScreen implements Screen {
 
     private ExtendViewport extendViewport;
 
-    private World world;
-    private Box2DDebugRenderer box2dDebugRenderer;
-    private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
+    private float speed = 300f;
+
     private LevelUtil levelUtil;
+    private World world;
+    private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
+    private Box2DDebugRenderer box2dDebugRenderer;
 
     public GameScreen(final RonaSurvivors game) {
         this.game = game;
@@ -40,37 +42,51 @@ public class GameScreen implements Screen {
         inputUtil = new InputUtil();
 
         // Tiles
-        world = new World(new Vector2(0,0), false);
+        world = new World(new Vector2(0,0), true);
         box2dDebugRenderer = new Box2DDebugRenderer();
         levelUtil = new LevelUtil();
-        orthogonalTiledMapRenderer = levelUtil.setupMap("maps/map0.tmx");
+        levelUtil.LoadTileMap("maps/debugLevel1/map0.tmx");
+        orthogonalTiledMapRenderer = levelUtil.GetTileRenderer();
+        levelUtil.GetLayers();
     }
 
     @Override
     public void show() {}
 
     private void update() {
+        // Update physics
         world.step(1/60f, 6, 2);
-        extendViewport.apply();  /* Not implemented yet (camera follows player) */
 
-       
-        game.batch.setProjectionMatrix(extendViewport.getCamera().combined);
-        orthogonalTiledMapRenderer.setView((OrthographicCamera) extendViewport.getCamera());
-        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
-            Gdx.app.exit();
-        }
+        // Move camera
+        Vector3 camPos = extendViewport.getCamera().position;
+        extendViewport.getCamera().position.set(
+            camPos.x + inputUtil.moveX() * speed * Gdx.graphics.getDeltaTime(),
+            camPos.y + inputUtil.moveY() * speed * Gdx.graphics.getDeltaTime(),
+            camPos.z
+        );
+        // Update camera
+        extendViewport.getCamera().update();
     }
 
     @Override
     public void render(float delta) {
-        this.update();
+        // Clear screen
+        ScreenUtils.clear(Color.BLACK);
         
-        ScreenUtils.clear(Color.CYAN);
-
-        // Input
+        // Updates
         inputUtil.update();
 
+        // Setup Render
+        extendViewport.apply();
+
+        // Run physics loop
+        this.update();
+
+        // Render
+        game.batch.setProjectionMatrix(extendViewport.getCamera().combined);
+        orthogonalTiledMapRenderer.setView((OrthographicCamera) extendViewport.getCamera());
         orthogonalTiledMapRenderer.render();
+
         box2dDebugRenderer.render(world, extendViewport.getCamera().invProjectionView);
     }
 
@@ -89,9 +105,6 @@ public class GameScreen implements Screen {
     public void hide() {}
 
     @Override
-    public void dispose() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dispose'");
-    }
+    public void dispose() {}
     
 }
--- core/src/dev/krirogn/ronasurvivors/Utils/LevelUtil.java
+++ core/src/dev/krirogn/ronasurvivors/Utils/LevelUtil.java
@@ -1,5 +1,8 @@
 package dev.krirogn.ronasurvivors.Utils;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapLayers;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -7,12 +10,6 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 public class LevelUtil {
     
     private TiledMap tiledMap;
-
-    public OrthogonalTiledMapRenderer setupMap(String mapName){
-        tiledMap = new TmxMapLoader().load(mapName);
-        return new OrthogonalTiledMapRenderer(tiledMap);
-    }
-
     
     /**
      * Loads a tile map from a local asset file
@@ -21,13 +18,27 @@ public class LevelUtil {
      * *.tmx file.
      * <p>
      * "assets/" is the path root
-     * @return a TiledMap object from the file
      * @see <a href="https://libgdx.com/wiki/graphics/2d/tile-maps">Tile Maps libGDX docs</a>
      * @see <a href="https://www.mapeditor.org/">Tiled Website</a>
      */
-    /*
-    public static TiledMap LoadTileMap(String tilePath) {
-        return new TmxMapLoader().load(tilePath);
+    public void LoadTileMap(String tilePath) {
+        tiledMap = new TmxMapLoader().load(tilePath);
     }
-    */
+
+    public void GetLayers() {
+        MapLayers ml = tiledMap.getLayers();
+        
+        for (MapLayer layer : ml)
+            Gdx.app.debug("Tiles", layer.getName());
+    }
+
+    /**
+     * Gives the tile map as an
+     * OrthogonalTiledMapRenderer
+     * 
+     * @return and OrthogonalTiledMapRenderer
+     */
+    public OrthogonalTiledMapRenderer GetTileRenderer() {
+        return new OrthogonalTiledMapRenderer(tiledMap);
+    }
 }
