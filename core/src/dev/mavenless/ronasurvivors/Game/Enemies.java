6596d952d3e60ab6d0ccf954ce85f1d7003065e1
Tobias.Husebo <tobias.husebo@student.uib.no>
2023-03-09 12:02:11 +0100

    changed name (Enemies -> Enemy.java)

---
+++ core/src/dev/mavenless/ronasurvivors/Game/Enemies.java
@@ -0,0 +1,16 @@
+package dev.mavenless.ronasurvivors.Game;
+
+import dev.mavenless.ronasurvivors.Utils.LevelUtil;
+
+public class Enemy {
+    private LevelUtil levelUtil;
+
+    private Rectangle size;
+    private Sprite sprite;
+    private float speed;
+    private Body body;
+
+    public Enemy(Rectangle size, Sprite sprite, float speed, Body body, LevelUtil levelUtil) {
+
+    }
+}
