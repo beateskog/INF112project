a90bbf123e004885d651acda98dc672d3d318ce0
krirogn <kf@krirogn.dev>
2023-03-10 15:10:01 +0100

    JUnit

---
+++ core/src/test/mavenless/ronasurvivors/RonaSurvivorsTest.java
@@ -0,0 +1,19 @@
+package test.mavenless.ronasurvivors;
+
+
+import dev.mavenless.ronasurvivors.RonaSurvivors;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RonaSurvivorsTest {
+    
+    @Test
+    public void firstTest() {
+        // RonaSurvivors rs = new RonaSurvivors();
+        // assertEquals(rs.batch, null);
+
+        assertEquals(1, 2);
+    }
+
+}
