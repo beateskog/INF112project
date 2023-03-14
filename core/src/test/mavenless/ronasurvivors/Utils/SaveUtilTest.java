17c25eb24c394b52021440c082394b2d3d3c62e8
krirogn <kf@krirogn.dev>
2023-03-14 18:45:29 +0100

    SaveUtil tests

--- core/src/test/mavenless/ronasurvivors/Utils/SaveUtilTest.java
+++ core/src/test/mavenless/ronasurvivors/Utils/SaveUtilTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.Serializable;
 import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Objects;
 
 import org.junit.jupiter.api.AfterAll;
 
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 
@@ -20,12 +23,12 @@ import mavenless.ronasurvivors.HeadlessApp;
 public class SaveUtilTest extends ApplicationAdapter {
 
     private static HeadlessApplication headlessApplication = null;
-
     private static String tmpDir = null;
 
+    // libGDX and temp dir setup
     @BeforeAll
     public static void start() {
-        System.out.println("Setting up SaveUtilTest");
+        System.out.println("Starting SaveUtilTest");
 
         // Set up headless application
         if (headlessApplication == null) {
@@ -42,15 +45,22 @@ public class SaveUtilTest extends ApplicationAdapter {
             Assertions.fail("Couldn't make a temp dir");
             return;
         }
-        System.out.println(tmpDir);
+        System.out.println("Temp dir is: " + tmpDir);
     }
 
+    // libGDX and temp dir cleanup
     @AfterAll
     public static void end() {
         System.out.println("Ending SaveUtilTest");
 
         // Remove temp dir
-        Gdx.files.absolute(tmpDir).deleteDirectory();
+        Assertions.assertTrue(
+            Gdx.files.absolute(tmpDir).deleteDirectory(),
+            "Couldn't delete the temp dir"
+        );
+
+        // Quit libGDX
+        Gdx.app.exit();
     }
 
     @Test
@@ -73,6 +83,85 @@ public class SaveUtilTest extends ApplicationAdapter {
             Arrays.equals(Gdx.files.absolute(tmpPath).readBytes(), bytes),
             "The written bytes are not the same as the read ones"
         );
+    }
+
+    @Test
+    public void testByteRead() {
+        // Test data
+        byte[] bytes = { 'M', 'E', 'L', 'O', 'N' };
+        
+        // Get the test file path
+        String tmpPath = tmpDir + "testByteRead.test";
+
+        // Check that write works
+        try {
+            Gdx.files.absolute(tmpPath).writeBytes(bytes, false);
+        } catch (GdxRuntimeException e) {
+            Assertions.fail("Write Error: " + e.getMessage());
+        }
+
+        // Check that the read bytes are correct
+        Assertions.assertTrue(
+            Arrays.equals(SaveUtil.readBytesFromFile(tmpPath), bytes),
+            "The written bytes are not the same as the read ones"
+        );
+    }
+
+    @Test
+    public void testObjectWriteRead() {
+        // Test data
+        TestData testData = new TestData("Coconut", 537.88256f);
+        
+        // Get the test file path
+        String tmpPath = tmpDir + "testObjectWrite.test";
+
+        // Check that write is successfull
+        try {
+            SaveUtil.writeObject("JTS", (short) 2, testData, tmpPath);
+        } catch (Exception e) {
+            Assertions.fail("Write Error: " + e.getMessage());
+        }
+
+        // Check that the read bytes are correct
+        try {
+            TestData readData = (TestData) SaveUtil.readObject("JTS", (short) 2, tmpPath);
+            Assertions.assertTrue(readData.equals(testData));
+        } catch (Exception e) {
+            Assertions.fail("Read Error: " + e.getMessage());
+        }
+    }
+
+}
+
+
+class TestData implements Serializable {
+
+    public String name;
+    public float value;
+
+    public TestData(String name, float value) {
+        this.name = name;
+        this.value = value;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        // Self check
+        if (this == obj)
+            return true;
+
+        // Null check
+        if (obj == null)
+            return false;
+
+        // Type check and cast
+        if (getClass() != obj.getClass())
+            return false;
+
+        TestData testData = (TestData) obj;
+
+        // Field comparison
+        return Objects.equals(name, testData.name) && Objects.equals(value, testData.value);
     }
 
 }
