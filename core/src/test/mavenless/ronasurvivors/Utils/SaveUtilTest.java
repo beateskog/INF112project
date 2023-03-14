09c10774bc15f1ea292c014aacc9a2d603ad8e0b
krirogn <kf@krirogn.dev>
2023-03-14 15:19:16 +0100

    SaveUtil tests

--- core/src/test/mavenless/ronasurvivors/Utils/SaveUtilTest.java
+++ core/src/test/mavenless/ronasurvivors/Utils/SaveUtilTest.java
@@ -3,19 +3,25 @@ package mavenless.ronasurvivors.Utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.AfterAll;
 
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 
 import mavenless.ronasurvivors.HeadlessApp;
 
 public class SaveUtilTest extends ApplicationAdapter {
 
     private static HeadlessApplication headlessApplication = null;
+
+    private static String tmpDir = null;
 
     @BeforeAll
     public static void start() {
@@ -28,35 +34,45 @@ public class SaveUtilTest extends ApplicationAdapter {
             Assertions.assertNotNull(Gdx.files);
         }
 
-        // Get the save location
-        String saveLocation = SaveUtil.getDefaultSavePath();
-
-        // Check if the save location dir exists
-        FileHandle saveLocationHandle = Gdx.files.absolute(saveLocation);
-        if (!saveLocationHandle.exists()) saveLocationHandle.mkdirs();
-
-        // Check if the tests dir exists
-        String saveLocationTests = saveLocation + "tests/";
-        FileHandle saveLocationTestsHandle = Gdx.files.absolute(saveLocationTests);
-        if (!saveLocationTestsHandle.exists()) saveLocationTestsHandle.mkdirs();
+        // Get temp directory
+        try {
+            tmpDir = Files.createTempDirectory("rona-survivors").toFile().getAbsolutePath() + "/";
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            Assertions.fail("Couldn't make a temp dir");
+            return;
+        }
+        System.out.println(tmpDir);
     }
 
     @AfterAll
     public static void end() {
         System.out.println("Ending SaveUtilTest");
+
+        // Remove temp dir
+        Gdx.files.absolute(tmpDir).deleteDirectory();
     }
 
     @Test
     public void testByteWrite() {
-        // byte[] bytes = { 'T', 'E', 'S', 'T' };
-        // String tmpPath = SaveUtil.getDefaultSavePath() + "tests/saveUtilTest.test";
+        // Test data
+        byte[] bytes = { 'T', 'E', 'S', 'T' };
+        
+        // Get the test file path
+        String tmpPath = tmpDir + "testByteWrite.test";
 
         // Check that write returns true
-        // Assertions.assertEquals(SaveUtil.writeBytesToFile(tmpPath, bytes), true);
+        Assertions.assertEquals(
+            SaveUtil.writeBytesToFile(tmpPath, bytes),
+            true,
+            "The file couldn't be written"
+        );
 
-        // // Check that the read bytes are correct
-        // Assertions.assertEquals(bytes, Gdx.files.absolute(tmpPath).readBytes());
-        Assertions.assertTrue(true);
+        // Check that the read bytes are correct
+        Assertions.assertTrue(
+            Arrays.equals(Gdx.files.absolute(tmpPath).readBytes(), bytes),
+            "The written bytes are not the same as the read ones"
+        );
     }
 
 }
