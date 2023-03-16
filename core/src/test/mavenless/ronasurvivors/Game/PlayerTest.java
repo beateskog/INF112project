b2a91edc3fd2ccc309567c27033a69d48ef0c7c9
Kristian.Skeie <kristian.skeie@student.uib.no>
2023-03-16 13:07:37 +0100

    Added PlayerTest.java for testing Player.java

---
+++ core/src/test/mavenless/ronasurvivors/Game/PlayerTest.java
@@ -0,0 +1,15 @@
+package mavenless.ronasurvivors.Game;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+public class PlayerTest {
+    
+    @Test
+    public void testTextureRegion(){
+        
+    }
+}
