1a10b7c17667b27c1f7c8d888905d39e6cbc6cc7
Beate Skogvik <beate.skogvik@student.uib.no>
2023-03-21 15:23:35 +0100

    removed unused imports

--- core/src/dev/mavenless/ronasurvivors/Game/Projectile.java
+++ core/src/dev/mavenless/ronasurvivors/Game/Projectile.java
@@ -1,13 +1,9 @@
 package mavenless.ronasurvivors.Game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.ai.fsm.State;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
