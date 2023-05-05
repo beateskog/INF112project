0f8db00a536c2455a8730a69d5f18bf732a22501
Beate Skogvik <beate.skogvik@student.uib.no>
2023-05-05 10:06:37 +0200

    Added header

--- core/src/dev/mavenless/ronasurvivors/Screens/MainMenuScreen.java
+++ core/src/dev/mavenless/ronasurvivors/Screens/MainMenuScreen.java
@@ -2,9 +2,12 @@ package mavenless.ronasurvivors.Screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -19,6 +22,8 @@ public class MainMenuScreen implements Screen {
     private Stage stage;
     private RonaSurvivors game;
 
+    private Label header = new Label("Rona Survivors", new Label.LabelStyle(new BitmapFont(), Color.BLACK));
+
     public MainMenuScreen(RonaSurvivors game){
         this.game = game;
         create();
@@ -28,6 +33,9 @@ public class MainMenuScreen implements Screen {
         stage = new Stage(new ScreenViewport());
         skin = new Skin(Gdx.files.internal("ui/Menu/MainMenuScreen.json"));
         Gdx.input.setInputProcessor(stage);
+        
+
+        header.setFontScale(8f, 5f);
 
         Table table = new Table();
         table.setBackground(skin.getDrawable("Group 1"));
@@ -87,12 +95,14 @@ public class MainMenuScreen implements Screen {
     public void render(float arg0) {
         Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.addActor(header);
         stage.act();
         stage.draw();
     }
 
     public void resize(int width, int height) {
         stage.getViewport().update(width, height, true);
+        header.setPosition(stage.getWidth() / 4 - header.getWidth() / 2, stage.getHeight() - header.getHeight() * 10);
     }
 
     public void dispose() {
