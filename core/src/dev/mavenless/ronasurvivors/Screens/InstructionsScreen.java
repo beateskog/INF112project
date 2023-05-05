2a53056968f4a77da30ce7cf7976c0fa80db953e
@Askild.Heiret <yud015@uib.no>
2023-05-05 10:56:09 +0200

    InstructionsScreen

--- core/src/dev/mavenless/ronasurvivors/Screens/InstructionsScreen.java
+++ core/src/dev/mavenless/ronasurvivors/Screens/InstructionsScreen.java
@@ -3,9 +3,12 @@ package mavenless.ronasurvivors.Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import mavenless.ronasurvivors.RonaSurvivors;
@@ -30,7 +33,24 @@ public class InstructionsScreen implements Screen{
         table.setBackground(skin.getDrawable("Group 1"));
         table.setFillParent(true);
 
+        table.add(backButton());
+
         stage.addActor(table);
+    }
+
+
+    private TextButton backButton(){
+        TextButton back = new TextButton(null, skin);
+        back.setText("BACK");
+        back.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.debug("UI", "back: " + back.isChecked());
+                game.setScreen(new MainMenuScreen(game));
+                dispose();
+            }
+        });
+        return back;
     }
 
     @Override
